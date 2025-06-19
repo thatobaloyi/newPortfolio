943f19926575060afd8bade897884d4c67d06813
Thato Baloyi <thatobaloyi@Thatos-MacBook-Air.local>
2025-06-19 03:54:27 +0200

    modified the header, put backdrop blur filter

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,12 +3,15 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header>
+    <header className="fixed w-screen left-0 top-0 m-auto">
+        <div className="flex shadow justify-between mx-15 lg:mx-100 mt-5 px-5 rounded-full backdrop">   
+        <Link href="/" className="font-bold flex items-center">Thato Baloyi</Link>
         <nav className="flex gap-2">
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
         </nav>
+        </div>
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import WorkExperience from "./components/WorkExperience";
 import Education from "./components/Education";
-
+import Landing from "./components/Landing";
 export default function Home() {
   return (
     <>
+      <Landing/>
       <AboutMe/>
+      {/* <hr /> */}
       <Skills/>
+      {/* <hr /> */}
       <WorkExperience/>
+      {/* <hr /> */}
       <Education/>
     </>
   );
